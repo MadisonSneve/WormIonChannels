1e97fe3de36285017ea8de18c8af625ec375247c
Orhan Celiker <otceliker@gmail.com>
2020-12-15 14:53:16 -0500

    fixed scoping bug in main_scipy; added SHL-1 fig

--- main_scipy.py
+++ main_scipy.py
@@ -7,37 +7,47 @@ from scipy import integrate
 def SHL1(y, t, voltage):
 
     mV = 1e-3; ms = 1e-3
-    # updated version
-    m_v_05 = 11.2 * mV #potentially -6.8
-    k_a = 14.1 * mV
-    m_SHL1_inf = lambda V : (1)/(1+np.exp(-(V-m_v_05)/k_a))
+    def m_SHL1_inf(V):
+        # updated version
+        m_v_05 = 11.2 * mV #potentially -6.8
+        k_a = 14.1 * mV
+        return (1)/(1+np.exp(-(V-m_v_05)/k_a))
 
-    a = 13.8 * ms #potentially 1.4
-    a = 1.4 * ms
-    b = -17.5 * mV
-    c = 12.9 * mV
-    d = -3.7 * mV
-    e = 6.5 * mV
-    f = 1.9 * ms #potentially 0.2
-    f = 0.2 * ms
-    tau_m_SHL1 = lambda V: a/(np.exp(-(V-b)/c) + np.exp((V-d)/e)) + f
+    def tau_m_SHL1(V):
+        a = 13.8 * ms #potentially 1.4
+        a = 1.4 * ms
+        b = -17.5 * mV
+        c = 12.9 * mV
+        d = -3.7 * mV
+        e = 6.5 * mV
+        f = 1.9 * ms #potentially 0.2
+        f = 0.2 * ms
+        return a/(np.exp(-(V-b)/c) + np.exp((V-d)/e)) + f
 
-    h_v_05 = -33.1 * mV
-    k_i = 8.3 * mV
-    h_f_SHL1_inf = lambda V: (1)/(1+np.exp((V-h_v_05)/k_i))
-    h_s_SHL1_inf = lambda V: (1)/(1+np.exp((V-h_v_05)/k_i))
+    def h_f_SHL1_inf(V):
+        h_v_05 = -33.1 * mV
+        k_i = 8.3 * mV
+        return (1)/(1+np.exp((V-h_v_05)/k_i))
 
-    a = 539.2 * ms #potentially 53.9
-    b = -28.2 * mV
-    c = 4.9 * mV
-    d = 27.3 * ms #potentially 2.7
-    tau_h_f_SHL1 = lambda V: ((a)/(1 + np.exp((V-b)/c))) + d
+    def h_s_SHL1_inf(V):
+        h_v_05 = -33.1 * mV
+        k_i = 8.3 * mV
+        return (1)/(1+np.exp((V-h_v_05)/k_i))
 
-    a = 8422.0 * ms #potentially 842.2
-    b = -37.7 * mV
-    c = 6.4 * mV
-    d = 118.9 * ms #potentially 11.9
-    tau_h_s_SHL1 = lambda V: ((a)/(1 + np.exp((V-b)/c))) + d
+    def tau_h_f_SHL1(V):
+        a = 539.2 * ms #potentially 53.9
+        b = -28.2 * mV
+        c = 4.9 * mV
+        d = 27.3 * ms #potentially 2.7
+        return ((a)/(1 + np.exp((V-b)/c))) + d
+
+    def tau_h_s_SHL1(V):
+        a = 8422.0 * ms #potentially 842.2
+        b = -37.7 * mV
+        c = 6.4 * mV
+        d = 118.9 * ms #potentially 11.9
+        return ((a)/(1 + np.exp((V-b)/c))) + d
+
     #I_SHL1 = g_SHL1 * (m_SHL1)**3 * (0.7 * h_f_SHL1 + 0.3 * h_s_SHL1) * (V - V_K)
 
     m_SHL1 = y[0]
@@ -74,14 +84,14 @@ def SHL1(y, t, voltage):
     dh_f_SHL1_dt = (h_f_SHL1_inf(voltage)-h_f_SHL1)/tau_h_f_SHL1(voltage)
     dh_s_SHL1_dt = (h_s_SHL1_inf(voltage)-h_s_SHL1)/tau_h_s_SHL1(voltage)
 
-    vv = np.linspace(-100*1e-3,70*1e-3,1000)
-    hinf = h_f_SHL1_inf(vv)
-    taum = tau_m_SHL1(vv)
-    minf = m_SHL1_inf(vv)
+    #vv = np.linspace(-100*1e-3,70*1e-3,1000)
+    #hinf = h_f_SHL1_inf(vv)
+    #taum = tau_m_SHL1(vv)
+    #minf = m_SHL1_inf(vv)
     #plt.plot(vv, hinf)
     #plt.plot(vv, minf)
-    plt.plot(vv, taum)
-    plt.show()
+    #plt.plot(vv, taum)
+    #plt.show()
 
 
     #I_SHL1 = g_SHL1 * m_3_SHL1 * (0.7 * h_f_SHL1 + 0.3 * h_s_SHL1) * (V - V_K)
@@ -91,36 +101,48 @@ def SHL1(y, t, voltage):
 
 init_values = [0.00154979, 0.99648898, 0.99648898]
 
-t0 = 0
-t1 = 100
+def run_experiment(V_pre,V_post):
+    t0 = 0e-3
+    t1 = 100e-3
+    t2 = 700e-3
 
-V_pre = -80e-3
-V_post = 0e-3
+    tsteps1 = int((t1-t0)*1e6)
+    tsteps2 = int((t2-t1)*1e6)
 
-result_pre = integrate.odeint(SHL1, np.asarray(init_values), np.linspace(t0,t1,(t1-t0)*1000), args=(V_pre,))
-print("Steady state:")
-print(result_pre[-1,:])
-t2 = 200
-result_post = integrate.odeint(SHL1, result_pre[-1,:], np.linspace(t1,t2,(t2-t1)*1000),args=(V_post,))
+    result_pre = integrate.odeint(SHL1, np.asarray(init_values), np.linspace(t0,t1,tsteps1), args=(V_pre,))
+    print("Steady state:")
+    print(result_pre[-1,:])
+    result_post = integrate.odeint(SHL1, result_pre[-1,:], np.linspace(t1,t2,tsteps2),args=(V_post,))
 
-m_SHL1_pre = result_pre[:,0]
-h_f_SHL1_pre = result_pre[:,1]
-h_s_SHL1_pre = result_pre[:,2]
+    m_SHL1_pre = result_pre[:,0]
+    h_f_SHL1_pre = result_pre[:,1]
+    h_s_SHL1_pre = result_pre[:,2]
 
-m_SHL1_post = result_post[:,0]
-h_f_SHL1_post = result_post[:,1]
-h_s_SHL1_post = result_post[:,2]
+    m_SHL1_post = result_post[:,0]
+    h_f_SHL1_post = result_post[:,1]
+    h_s_SHL1_post = result_post[:,2]
 
-if True:
-    g_SHL1 = 1.8 * 1e-9
-    V_K = -80*1e-3
-    I_SHL1_pre = np.transpose(g_SHL1 * np.power(m_SHL1_pre,3) * (0.7 * h_f_SHL1_pre + 0.3 * h_s_SHL1_pre) * (-80*1e-3 - V_K))
-    I_SHL1_post = np.transpose(g_SHL1 * np.power(m_SHL1_post,3) * (0.7 * h_f_SHL1_post + 0.3 * h_s_SHL1_post) * (40*1e-3 - V_K))
+    if True:
+        g_SHL1 = 1.8 * 1e-9
+        V_K = -80*1e-3
+        I_SHL1_pre = np.transpose(g_SHL1 * np.power(m_SHL1_pre,3) * (0.7 * h_f_SHL1_pre + 0.3 * h_s_SHL1_pre) * (V_pre - V_K))
+        I_SHL1_post = np.transpose(g_SHL1 * np.power(m_SHL1_post,3) * (0.7 * h_f_SHL1_post + 0.3 * h_s_SHL1_post) * (V_post - V_K))
 
-    print(I_SHL1_pre)
-    print()
-    print(I_SHL1_post)
-    plt.plot(np.linspace(t0,t2,(t2-t0)*1000),np.hstack((I_SHL1_pre, I_SHL1_post)))
-    plt.xlabel("Time (ms)")
-    plt.ylabel("I_SHL1 (arbitrary units)")
-    plt.show()
+        print(I_SHL1_pre)
+        print()
+        print(I_SHL1_post)
+        plt.plot(np.linspace(t0,t2,tsteps1+tsteps2),np.hstack((I_SHL1_pre, I_SHL1_post)))
+        plt.xlabel("Time (ms)")
+        plt.ylabel("I_SHL1 (arbitrary units)")
+
+run_experiment(-80e-3, 40e-3)
+run_experiment(-80e-3, 30e-3)
+run_experiment(-80e-3, 20e-3)
+run_experiment(-80e-3, 10e-3)
+run_experiment(-80e-3, 0e-3)
+run_experiment(-80e-3, -10e-3)
+run_experiment(-80e-3, -20e-3)
+run_experiment(-80e-3, -30e-3)
+run_experiment(-80e-3, -40e-3)
+
+plt.show()
